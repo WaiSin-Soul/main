36d9018bef27921410427667afe43b48b8202892
Vincent <vincentli.dev@gmail.com>
2025-01-18 21:17:58 -0500

    added icons

--- waisin-soul-fe/src/app/components/Header.tsx
+++ waisin-soul-fe/src/app/components/Header.tsx
@@ -28,7 +28,10 @@ const Header = () => {
                     <ul className="flex space-x-6">
                         <li className="relative">
                             <button onClick={toggleDropdown} className="hover:underline focus:outline-none">
-                                Art Collection
+                                <div className="flex flex-row">
+                                    Art Collection
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/></svg>
+                                </div>
                             </button>
                             {isDropdownOpen && (
                                 <ul className="absolute left-0 mt-2 bg-white text-black shadow-lg rounded-md z-10">
@@ -43,14 +46,14 @@ const Header = () => {
                         <li><Link href="/coaching" className="hover:underline">Coaching Services</Link></li>
                         <li><Link href="/products" className="hover:underline">Products</Link></li>
                         <li><Link href="/contact" className="hover:underline">Contact Me</Link></li>
-                        <li><Link href="/about" className="hover:underline">About Us</Link></li>
+                        <li><Link href="/about" className="hover:underline">About Me</Link></li>
                     </ul>
                 </nav>
             </div>
             <div className="flex space-x-4">
-                <button className="hover:underline">Search</button>
-                <Link href="/login" className="hover:underline">Login</Link>
-                <Link href="/cart" className="hover:underline">Cart</Link>
+                <button className="hover:underline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/></svg></button>
+                <Link href="/login" className="hover:underline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/></svg></Link>
+                <Link href="/cart" className="hover:underline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/></svg></Link>
             </div>
         </header>
     );
