e6241ce0e21832e65bdb255328c2d20fd9b1d6cb
Vivillia <vivillia.luo@gmail.com>
2025-01-18 21:03:50 -0500

    art-collection routing

--- waisin-soul-fe/src/app/components/Header.tsx
+++ waisin-soul-fe/src/app/components/Header.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link';
 const Header = () => {
     return (
         <header className="flex justify-between items-center p-4 bg-background text-accent">
-            <h1 className="text-xl"><Link href="/">LOGO</Link></h1>
+            <h1 className="text-xl"><Link href="/"></Link></h1>
             <nav>
                 <ul className="flex space-x-4">
-                    <li><Link href="/art" className="hover:underline">Art Collection</Link></li>
+                    <li><Link href="/art-collection" className="hover:underline">Art Collection</Link></li>
                     <li><Link href="/coaching" className="hover:underline">Coaching Services</Link></li>
                     <li><Link href="/products" className="hover:underline">Products</Link></li>
                     <li><Link href="/contact" className="hover:underline">Contact Me</Link></li>
