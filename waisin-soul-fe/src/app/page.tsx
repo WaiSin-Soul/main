7ea4e46eaf44610a1e97858feb5353f5de72ab91
Vincent <vincentli.dev@gmail.com>
2025-01-18 21:33:30 -0500

    cleaned up home page

--- waisin-soul-fe/src/app/page.tsx
+++ waisin-soul-fe/src/app/page.tsx
@@ -82,34 +82,6 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="w-full lg:w-full">
-          <Carousel />
-        </div>
-        
-        <Card
-          title={largeCard.title}
-          description={largeCard.description}
-          imageSrc={largeCard.imageSrc}
-          link={largeCard.link}
-        />
-
-
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          {cards.map((card, index) => (
-            <Card
-            key={index}
-            title={card.title}
-            description={card.description}
-            imageSrc={card.imageSrc}
-            link={card.link}
-            />
-          ))}
-        </div>
-        <FeaturedCollection items={featuredItems} />
-
-      
-      </main> */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="w-full lg:w-3/4">
           <Carousel />
