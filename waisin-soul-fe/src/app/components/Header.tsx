9c2d3e72e1ce578b7a3f7243765640a0d9e0e36d
Vivillia <vivillia.luo@gmail.com>
2025-02-12 12:28:23 -0500

    fix header

--- waisin-soul-fe/src/app/components/Header.tsx
+++ waisin-soul-fe/src/app/components/Header.tsx
@@ -22,17 +22,12 @@ const Header = () => {
     };
 
     return (
-        <header className="flex justify-between items-center p-4 bg-header text-white relative">
-            <div className="flex items-center space-x-8 mx-auto">
-                <button onClick={toggleMenu} className="md:hidden focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                    </svg>
-                </button>
+        <header className="flex justify-between items-center p-5 bg-header text-white relative">
+            <div className="flex items-center gap-x-8 mx-32">
                 <h1 className="text-xl font-bold">
                     <Link href="/">
                         <Image
-                            className="w-1/2 h-auto object-cover"
+                            className="w-3/4 h-auto object-cover"
                             src="/images/waisin_soul_logo.png"
                             alt="logo"
                             width={600}
@@ -40,13 +35,18 @@ const Header = () => {
                         />
                     </Link>
                 </h1>
+                <button onClick={toggleMenu} className="md:hidden focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                    </svg>
+                </button>
             </div>
             <nav className={`md:flex ${isMenuOpen ? 'block' : 'hidden'} absolute md:static bg-header w-full md:w-auto top-16 left-0`}>
                 <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 p-4 md:p-0">
                     <li className="relative">
                         <button onClick={toggleDropdown} className="hover:underline focus:outline-none">
                             <div className="flex flex-row">
-                                Art Collection
+                                <span className="text-lg">Art Collection</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
                                 </svg>
@@ -62,13 +62,13 @@ const Header = () => {
                             </ul>
                         )}
                     </li>
-                    <li><Link href="/coaching" className="hover:underline">Coaching Services</Link></li>
-                    <li><Link href="/products" className="hover:underline">Products</Link></li>
-                    <li><Link href="/contact" className="hover:underline">Contact Me</Link></li>
-                    <li><Link href="/about" className="hover:underline">About Me</Link></li>
+                    <li><Link href="/coaching" className="hover:underline"><span className="text-lg">Coaching</span></Link></li>
+                    <li><Link href="/products" className="hover:underline"><span className="text-lg">Products</span></Link></li>
+                    <li><Link href="/contact" className="hover:underline"><span className="text-lg">Contact Me</span></Link></li>
+                    <li><Link href="/about" className="hover:underline"><span className="text-lg">About Me</span></Link></li>
                 </ul>
             </nav>
-            <div className="flex space-x-4">
+            <div className="flex space-x-6 pr-5">
                 <button className="hover:underline" onClick={toggleSearch}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/></svg></button>
                 <Link href="/login" className="hover:underline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/></svg></Link>
                 <Link href="/cart" className="hover:underline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/></svg></Link>
